5620c3e5eb512c8d922381c5f8deea4f73129cbb
Fellipe Rocha <felliperocha464@gmail.com>
2025-11-14 20:49:45 -0300

    ajuste finais

--- TicketFlowWeb/obj/Debug/net9.0/TicketFlowWeb.AssemblyInfo.cs
+++ TicketFlowWeb/obj/Debug/net9.0/TicketFlowWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketFlowWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b9f4a3bd31ead727c41bb64e5188ebedcf4f76f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7edaf69a7fb099096c1230064379075a49e8a58")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketFlowWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketFlowWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
